08bee4024cbb39c023b74e31046e977596be4297
Kevin Becker <kevin@kevinandnell.com>
2021-11-03 07:13:25 -0400

    Update delete to use Modal confirmation and FA icon

--- ToDoApp/ClientApp/src/components/Delete.js
+++ ToDoApp/ClientApp/src/components/Delete.js
@@ -1,31 +1,57 @@
 ﻿import React, { Component } from "react";
-import { Button } from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 export class Delete extends Component {
   constructor(props) {
     super(props);
 
     this.handleDelete = this.handleDelete.bind(this);
+
+    this.state = {
+      modal: false,
+    };
   }
 
   handleDelete() {
     var p = this.props;
     var id = p.id;
 
-    if (window.confirm("Are you sure you want to delete this to do?")) {
-      fetch(`/api/ToDo/${id}`, {
-        method: "DELETE",
-      }).then((response) => {
-        if (response.status === 200) {
-          p.onDelete();
-        } else {
-          alert("There was a problem");
-        }
-      });
-    }
+    fetch(`/api/ToDo/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.status === 200) {
+        p.onDelete();
+      } else {
+        alert("There was a problem");
+      }
+    });
   }
 
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
   render() {
-    return <Button onClick={this.handleDelete}>Delete</Button>;
+    return (
+      <div>
+        <FontAwesomeIcon onClick={this.toggle} icon={faTrashAlt} />
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader>Delete</ModalHeader>
+          <ModalBody>Do you want to delete this to do?</ModalBody>
+          <ModalFooter>
+            <Button onClick={this.toggle} color="primary" outline>
+              Cancel
+            </Button>
+            <Button onClick={this.handleDelete} color="danger">
+              Confirm
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
   }
 }
